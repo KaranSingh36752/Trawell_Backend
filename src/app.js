0a2f22ebf9c75fb4c78117ebc1fcb21c6d83d4d4
karan <karansingh36752@gmail.com>
2024-10-05 14:47:46 +0530

    updating

--- src/app.js
+++ src/app.js
@@ -2,44 +2,56 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userID?/:name?/:password?", (req, res) => {
-    console.log(req.params)
-  res.send({ firstName: "Karan", lastName: "Singh", dept_name: "CSE" });
+app.get(
+  "/user/:userID?/:name?/:password?",
+  (req, res, next) => {
+    console.log(req.params);
+    const params = req.params;
+    next();
+    res.send(params);
+  },
+  (req, res, next) => {
+    console.log("handling the route 2nd");
+    res.send({ firstName: "Karan", lastName: "Singh", dept_name: "CSE" });
+  }
+);
+
+app.listen(7777, () => {
+  console.log("server is running on port 7777");
 });
 
-app.post("/user", (req, res) => {
-  res.send({
-    name: "John",
-    age: 30,
-    status: "single",
-  });
-});
 
-app.delete("/user", (req, res) => {
-  res.send("Deleted sucessfully");
-});
+// app.post("/user", (req, res) => {
+//   res.send({
+//     name: "John",
+//     age: 30,
+//     status: "single",
+//   });
+// });
 
-app.put("/user", (req, res) => {
-  res.send({
-    name: "John",
-    age: 30,
-    status: "married",
-  });
-});
+// app.delete("/user", (req, res) => {
+//   res.send("Deleted sucessfully");
+// });
 
-app.patch("/user", (req, res) => {
-  res.send({
-    status: "single",
-  });
-});
+// app.put("/user", (req, res) => {
+//   res.send({
+//     name: "John",
+//     age: 30,
+//     status: "married",
+//   });
+// });
+
+// app.patch("/user", (req, res) => {
+//   res.send({
+//     status: "single",
+//   });
+// });
 
 // app.use("/",(req,res)=>{
 //     res.send("Hello World");
 // })
 
-app.listen(7777, () => {
-  console.log("server is running on port 7777");
-});
+
 
 // app.use("/hello",(req,res)=>{
 //     res.send("heeloo  heeloejsja");
