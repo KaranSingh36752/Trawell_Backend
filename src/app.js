3de3c74e2be84483f2cbe9630b0e54078b8f7ffb
karan <karansingh36752@gmail.com>
2024-10-05 09:22:30 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res)=>{
+
+
+
+app.use("/hello",(req,res)=>{
+    res.send("heeloo  heeloejsja");
+})
+
+app.use("/hello/bitch",(req,res)=>{
+    res.send("Hello bitch");
+})
+app.use("/bitch",(req,res)=>{
+    res.send("STFU and leave");
+})
+app.use("/",(req,res)=>{
     res.send("Hello World");
 })
+
 
 app.listen(7777,()=>{
     console.log("server is running on port 7777");
