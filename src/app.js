9858dd5364b69c30905e1b06260cc586be56456d
karan <karansingh36752@gmail.com>
2024-10-08 00:11:37 +0530

    playing with middlewares

--- src/app.js
+++ src/app.js
@@ -1,25 +1,29 @@
 const express = require("express");
 
 const app = express();
+//So to handle the auth middlewares are used such that the repettion of code is not done again and again...
+app.use("/admin" ,(req,res,next)=>{
+    const token = "xyz";
+    const isAuthorized = token == "xyz";
+    if(!isAuthorized){
+        res.status(401).send("Not authorized token")
+    }
+    else next();
+})
 
-app.get(
-  "/user/:userID?/:name?/:password?",
-  (req, res, next) => {
-    console.log(req.params);
-    const params = req.params;
-    next();
-    res.send(params);
-  },
-  (req, res, next) => {
-    console.log("handling the route 2nd");
-    res.send({ firstName: "Karan", lastName: "Singh", dept_name: "CSE" });
-  }
-);
+
+app.get("/admin/getdata" , (req,res)=>{
+    res.send("admin data found");
+    
+})
+app.get("/admin/deletedata" , (req,res)=>{
+    res.send("admin data deleted");
+})
+
 
 app.listen(7777, () => {
   console.log("server is running on port 7777");
 });
-
 
 // app.post("/user", (req, res) => {
 //   res.send({
@@ -51,8 +55,6 @@ app.listen(7777, () => {
 //     res.send("Hello World");
 // })
 
-
-
 // app.use("/hello",(req,res)=>{
 //     res.send("heeloo  heeloejsja");
 // })
@@ -63,3 +65,34 @@ app.listen(7777, () => {
 // app.use("/bitch",(req,res)=>{
 //     res.send("STFU and leave");
 // })
+
+// },
+// (req, res, next) => {
+//   console.log("handling the route 2nd");
+//  // res.send({ firstName: "Karan", lastName: "Singh", dept_name: "CSE" });
+// }
+
+
+
+
+///HOW ROUTE HANDLERS ARE WORKING AND MIDDLEWARES ARE GENERATED WITH THIS FLOW
+// app.get(
+//   "/user/:userID?/:name?/:password?",
+//   (req, res, next) => {
+//     const params = req.params;
+//     res.send(params);
+//     next();
+//     console.log("handling the route 1nd");
+//     console.log(req.params);
+//   }
+// );
+// app.get(
+//     "/user/:userID?/:name?/:password?",
+//     (req, res, next) => {
+//       const params = req.params;
+//     //   res.send(params);
+//     //   console.log("handling the route 2nd");
+//     //   console.log(req.params);
+//     next();
+//     }
+//   );
