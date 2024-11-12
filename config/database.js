5467f7ae5453adfd4f67255ae5f071b52403712d
KaranSingh36752 <karansingh36752@gmail.com>
2024-11-12 22:50:23 +0530

    .env mongodb connection str

--- config/database.js
+++ config/database.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async() => {
     mongoose.connect(
-        "mongodb+srv://karansingh36752:dw0nWOUqPSk9n8aJ@nodejsnamaste.xfmxb.mongodb.net/Trawell"
+        "mongodb+srv://karansingh36752:FUtTnQBSdT12djOJ@nodejsnamaste.xfmxb.mongodb.net/Trawell"
     )
 }
 
 module.exports = connectDB;
+
