ef7b607f690fec957ebb2b81c6b53050221c559e
KaranSingh36752 <karansingh36752@gmail.com>
2024-11-17 19:07:49 +0530

    minor

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -4,7 +4,7 @@ const matchesRouter = express.Router();
 
 matchesRouter.post("/matches" , userAuth , async (req , res) => {
     const user = req.user;
-    console.log("sendong a connection req");
+    console.log("sending a connection req");
     res.send(user.firstName + " sent the friend request.");
   })
 
