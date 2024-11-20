7a8cc3f5f0c0b10337a7592dd06410a7df1a22d8
KaranSingh36752 <karansingh36752@gmail.com>
2024-11-21 01:18:32 +0530

    logout , user_update

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,60 +7,69 @@ const authRouter = express.Router();
 
 //Post the data with dynamic nature
 authRouter.post("/signup", async (req, res) => {
-    // VAlidation of signup user at api level validation
-    try {
-      validSignUpData(req);
-      const { firstName, lastName, age, emailId, gender, image, password } =
-        req.body; // Only
-      //Encryption of the password
-  
-      const passwordHash = await bcrypt.hash(password, 10);
-      // console.log(passwordHash);
-  
-      //i=new instance is created to  post the user
-      const newUser = new User({
-        firstName,
-        lastName,
-        age,
-        emailId,
-        gender,
-        image,
-        password: passwordHash,
-      });
-  
-      await newUser.save();
-      res.send("Data added successfully.");
-    } catch (err) {
-      res.status(400).send("ERROR :" + err.message);
-    }
-  });
+  // VAlidation of signup user at api level validation
+  try {
+    validSignUpData(req);
+    const { firstName, lastName, age, emailId, gender, image, password } =
+      req.body; // Only
+    //Encryption of the password
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    // console.log(passwordHash);
+
+    //i=new instance is created to  post the user
+    const newUser = new User({
+      firstName,
+      lastName,
+      age,
+      emailId,
+      gender,
+      image,
+      password: passwordHash,
+    });
+
+    await newUser.save();
+    res.send("Data added successfully.");
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
 
 authRouter.post("/login", async (req, res) => {
-    try {
-      const { emailId, password } = req.body;
-      if (!emailId) {
-        throw new Error("Email not present!!");
-      }
-  
-      const user = await User.findOne({ emailId });
-      if (!user) {
-        throw new Error("Invalid credentials!!");
-      }
-      const isValidPassword = await user.validatePassword(password);
-      if (isValidPassword) {
-        //token jwt
-        const token = await user.getJWT();
-        //console.log(token);
-        //Add THE token to the response BACK TO USER
-        res.cookie("token", token);
-        res.send("Login Successfull.");
-      } else {
-        throw new Error("Invalid credentials!!");
-      }
-    } catch (err) {
-      res.status(400).send("ERROR :" + err.message);
+  try {
+    const { emailId, password } = req.body;
+    if (!emailId) {
+      throw new Error("Email not present!!");
     }
-  });
-  
 
-module.exports = authRouter
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      throw new Error("Invalid credentials!!");
+    }
+    const isValidPassword = await user.validatePassword(password);
+    if (isValidPassword) {
+      //token jwt
+      const token = await user.getJWT();
+      //console.log(token);
+      //Add THE token to the response BACK TO USER
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      });
+      res.send("Login Successfull.");
+    } else {
+      throw new Error("Invalid credentials!!");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  res
+    .cookie("token", null, {
+      expires: new Date(Date.now()),
+    })
+    .send("Logout Successfull");
+});
+
+module.exports = authRouter;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const {userAuth} = require("../middlewares/auth.js")
- const userRouter = express.Router();
+const { userAuth } = require("../middlewares/auth.js");
+const userRouter = express.Router();
 userRouter.get("/user/:id", userAuth, async (req, res) => {
-    try {
-      // validate my token
-      const user = req.user;
-      res.send(user);
-    } catch (err) {
-      res.status(400).send("ERROR :" + err.message);
-    }
-  });
-  
+  try {
+    // validate my token
+    const user = req.user;
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
+
+userRouter.patch("/user/:id", userAuth, async (req, res) => {
+  try {
+    const ALLOWED_UPDATES = [""]
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
 
 module.exports = userRouter;
