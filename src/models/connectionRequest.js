174540ad9496f2d4d7a7d8195d8599397a1f14cf
KaranSingh36752 <karansingh36752@gmail.com>
2024-11-24 00:08:01 +0530

    Queries & indexes & handleding Corner cases

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -23,6 +23,8 @@ const connectionRequestSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+// indexes 
+connectionRequestSchema.index({fromUserId : 1 , toUserId : 1});
 
 connectionRequestSchema.pre("save" , function(){
   const connectionRequest = this;
