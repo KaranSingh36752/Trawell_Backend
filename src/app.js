01e8c57babf71ea13b43de989d8e409a7c20d64d
KaranSingh36752 <karansingh36752@gmail.com>
2025-02-05 17:03:05 +0530

    cors update

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     credentials: true,
   })
 );
