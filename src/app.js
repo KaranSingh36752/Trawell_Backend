71cac583bea54d1be0712c55dd7e80b97ee7e35a
KaranSingh36752 <karansingh36752@gmail.com>
2025-02-09 23:54:49 +0530

    minor

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   })
